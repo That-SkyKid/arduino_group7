025fd9f0fcebdd9d1699cfa9e0cf708c649a96cf
ThatSkyKid <35163124+That-SkyKid@users.noreply.github.com>
2023-12-29 02:33:20 +0800

    Update esp8766.ino

---
+++ ESP8766/esp8766/esp8766.ino
@@ -0,0 +1,33 @@
+#include <SoftwareSerial.h>
+#include <ESP8266_Lib.h>
+
+// Additonal Libary for Blynk
+
+SoftwareSerial EspSerial(2, 3); // RX, TX
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 9600
+
+ESP8266 wifi(&EspSerial);
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+  delay(10);
+  // First open a connection at 115200
+  EspSerial.begin(115200);
+  delay(10);
+  // Send AT command to set Wifi module's baudrate to 9600
+  EspSerial.println("AT+UART=9600,8,1,0,0");
+  delay(10);
+  // End the connection
+  EspSerial.end();
+  // Reopen the connection at 9600
+  EspSerial.begin(9600);
+  EspSerial.println("AT");
+}
+
+void loop()
+{
+}
