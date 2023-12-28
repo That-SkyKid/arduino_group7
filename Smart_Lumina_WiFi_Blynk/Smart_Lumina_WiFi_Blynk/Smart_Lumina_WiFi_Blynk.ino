7bc9b378336180c139eda7aa9e686b706476c64a
ThatSkyKid <35163124+That-SkyKid@users.noreply.github.com>
2023-12-29 03:01:07 +0800

    Update Smart_Lumina_WiFi_Blynk.ino

--- Smart_Lumina_WiFi_Blynk/Smart_Lumina_WiFi_Blynk/Smart_Lumina_WiFi_Blynk.ino
+++ Smart_Lumina_WiFi_Blynk/Smart_Lumina_WiFi_Blynk/Smart_Lumina_WiFi_Blynk.ino
@@ -20,10 +20,13 @@ char pass[] = "Abc2823356";
 
 // Software Serial on UNO Maker
 #include <SoftwareSerial.h>
-SoftwareSerial EspSerial(2, 3); // RX, TX
+SoftwareSerial EspSerial(8, 9); // RX, TX
 
 // Your ESP8266 baud rate:
-#define ESP8266_BAUD 115200
+#define ESP8266_BAUD 9600
+
+//Define LED on PIN 4
+#define LED 4
 
 ESP8266 wifi(&EspSerial);
 
@@ -32,23 +35,29 @@ BLYNK_WRITE(V0) //Listening V0 Pin...
 {
   int pinValue = param.asInt();
 
-  if(pinValue == 1){
-    digitalWrite(4,HIGH);
-    //do something when button is pressed (Open the LED)
-    } 
+  digitalWrite(LED, pinValue);
+}
+
+void setup()
+{
+  //LED as output
+  pinMode(LED, OUTPUT);
   
-  else if (pinValue == 0){
-    digitalWrite(4,LOW);
-    //do something else when button is pressed (Close the LED)
-  }
-  Serial.print("V0 Button value is: "); //Print value output to COM output
-  Serial.println(pinValue);  
+  // Debug console
+  Serial.begin(9600);
+
+  delay(10);
+
+  // Set ESP8266 baud rate
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, wifi, ssid, pass);
 }
 
 
 //For Mobile Dashboard Blynk
 BLYNK_WRITE(V1) // Listening V1 Pin ....
-
 {
   int pinValue = param.asInt(); //Assigning Incoming Value from V1 pin as variable
 
@@ -64,21 +73,6 @@ BLYNK_WRITE(V1) // Listening V1 Pin ....
 
   Serial.print("V1 Button value is: "); //Print value output to COM output
   Serial.println(pinValue);
-}
-
-void setup()
-
-{  
-  // Debug console
-  Serial.begin(9600);
-
-  delay(10);
-
-  // Set ESP8266 baud rate
-  EspSerial.begin(ESP8266_BAUD);
-  delay(10);
-
-  Blynk.begin(BLYNK_AUTH_TOKEN, wifi, ssid, pass);
 }
 
 void loop()
